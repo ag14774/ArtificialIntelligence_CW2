1b8ef45dbcd2d099436f5df303262a5eb945f5ca
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2017-03-31 17:03:04 +0100

    Ported code in assignment2_13798.pl for part 4

--- assignment2_13798.pl
+++ assignment2_13798.pl
@@ -6,54 +6,31 @@ solve_task(Task,Cost) :-
   ; part(4) -> solve_task_4(Task, Cost)
   ).
 
-%%%%%%%%%% Part 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+solve_task_path(CurPos,Task,Cost,Path,NewPos) :-
+  solve_task_path(CurPos,Task,Cost,Path,NewPos,true).
 solve_task_1(Task,Cost) :-
-  agent_current_position(oscar,P),
-  eval(Task,P,C),
-  insert_to_dict(visited{},P,C,VisitedInit),
-  % c(total, heuristic, depth, pos)
-  solve_task_bf(Task,[[c(C,C,0,P),P]],VisitedInit,R,Cost,_NewPos,false),!,  % prune choice point for efficiency
-  reverse(R,[_Init|Path]),
-  agent_do_moves(oscar,Path).
-%%%%%%%%%% Part 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  solve_task(Task,Cost,false).
 solve_task_3(Task,Cost) :-
-  agent_current_position(oscar,P),
-  eval(Task,P,C),
-  insert_to_dict(visited{},P,C,VisitedInit),
-  % c(total, heuristic, depth, pos)
-  solve_task_bf(Task,[[c(C,C,0,P),P]],VisitedInit,R,Cost,_NewPos,true),!,  % prune choice point for efficiency
-  reverse(R,[_Init|Path]),
-  agent_do_moves(oscar,Path).
+  solve_task(Task,Cost,true).
+solve_task_4(Task,Cost) :-
+  solve_task(Task,Cost,true).
 
-solve_task_3_path(CurPos,Task,Cost,Path,NewPos) :-
+solve_task(Task,Cost,LookAround) :-
+  current_position(P),
+  solve_task_path(P,Task,Cost,Path,_NewPos,LookAround),
+  do_moves(Path).
+
+solve_task_path(CurPos,Task,Cost,Path,NewPos,LookAround) :-
   eval(Task,CurPos,C),
   insert_to_dict(visited{},CurPos,C,VisitedInit),
   % c(total, heuristic, depth, pos)
-  solve_task_bf(Task,[[c(C,C,0,CurPos),CurPos]],VisitedInit,R,Cost,NewPos,true),!,  % prune choice point for efficiency
+  solve_task_bf(Task,[[c(C,C,0,CurPos),CurPos]],VisitedInit,R,Cost,NewPos,LookAround),!,  % prune choice point for efficiency
   reverse(R,[_Init|Path]).
-%%%%%%%%%% Part 4 (Optional) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-solve_task_4(Task,Cost):-
-  my_agent(Agent),
-  query_world( agent_current_position, [Agent,P] ),
-  solve_task_bt(Task,[c(0,P),P],0,R,Cost,_NewPos),!,  % prune choice point for efficiency
-  reverse(R,[_Init|Path]),
-  query_world( agent_do_moves, [Agent,Path] ).
-%%%%%%%%%% Part 4 (Optional) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% backtracking depth-first search, needs to be changed to agenda-based A*
-solve_task_bt(Task,Current,Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
-  achieved(Task,Current,RPath,Cost,NewPos).
-solve_task_bt(Task,Current,D,RR,Cost,NewPos) :-
-  Current = [c(F,P)|RPath],
-  search(P,P1,R,C),
-  \+ memberchk(R,RPath),  % check we have not been here already
-  D1 is D+1,
-  F1 is F+C,
-  solve_task_bt(Task,[c(F1,P1),R|RPath],D1,RR,Cost,NewPos).  % backtrack search
 
-%%%%%%%%% Part 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% A* search %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 solve_task_bf(Task,[Current|_Rest],_Visited,RPath,[cost(Cost),depth(Depth)],NewPos,Remember) :-
   achieved(Task,Current,RPath,Cost,Depth,NewPos,Remember).
 solve_task_bf(Task,[Current|Rest],Visited,RR,Cost,NewPos,Remember) :-
@@ -142,33 +119,13 @@ achieved(find(o(O)),Current,RPath,Cost,Depth,NewPos,true) :- !,
   Current = [c(Cost,_,Depth,NewPos)|RPath],
   RPath = [Last|_],
   look_around(Last,_,o(O)),
-  \+ agent_check_oracle(oscar,o(O)).
+  \+ check_oracle(o(O)).
 achieved(find(O),Current,RPath,Cost,Depth,NewPos,true) :-
   Current = [c(Cost,_,Depth,NewPos)|RPath],
   ( O=none    -> true
   ; otherwise -> RPath = [Last|_],look_around(Last,_,O)
   ).
 
-%%%%%%%%%%%%%%%%%%OLD ACHIEVED%%%%%%%%%%%%%%%%%%%%%%%
-
-achieved(go(Exit),Current,RPath,Cost,NewPos,_) :-
-  Current = [c(Cost,NewPos)|RPath],
-  ( Exit=none -> true
-  ; otherwise -> RPath = [Exit|_]
-  ).
-achieved(find(O),Current,RPath,Cost,NewPos,false) :-
-  Current = [c(Cost,NewPos)|RPath],
-  ( O=none    -> true
-  ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
-  ).
-achieved(find(O),Current,RPath,Cost,NewPos,true) :-
-  Current = [c(Cost,NewPos)|RPath],
-  ( O=none    -> true
-  ; otherwise -> RPath = [Last|_],look_around(Last,_,O)
-  ).
-
-search(F,N,N,1) :-
-  map_adjacent(F,N,empty).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 merge([], Agenda, Agenda).
@@ -183,3 +140,18 @@ merge([Child|Siblings],[First|Rest],[First|NewAgenda]) :-
 setof0(X,G,L) :-
   setof(X,G,L),!.
 setof0(_X,_G,[]).
+
+check_oracle(o(A)) :-
+  ( part(4) -> my_agent(Agent),query_world(agent_check_oracle,[Agent,o(A)])
+  ; otherwise -> agent_check_oracle(oscar,o(A))
+  ).
+
+current_position(P) :-
+  ( part(4) -> my_agent(Agent),query_world(agent_current_position,[Agent,P])
+  ; otherwise -> agent_current_position(oscar,P)
+  ).
+
+do_moves(Path) :-
+  ( part(4) -> my_agent(Agent),query_world(agent_do_moves,[Agent,Path])
+  ; otherwise -> agent_do_moves(oscar,Path)
+  ).
